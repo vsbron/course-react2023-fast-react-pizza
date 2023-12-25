f869c5378efae2562ef963280c898c415ca5fad8
vsbron <vsbron@gmail.com>
2023-12-25 15:14:55 +0300

    Added form handler via React Router action on /order/new/

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,9 @@ import Home from "./ui/Home";
 
 import Cart from "./features/cart/Cart";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
-import CreateOrder from "./features/order/CreateOrder";
+import CreateOrder, {
+  action as createOrderAction,
+} from "./features/order/CreateOrder";
 import Order, { loader as orderLoader } from "./features/order/Order";
 
 // Creating the routes
@@ -24,7 +26,11 @@ const router = createBrowserRouter([
         errorElement: <Error />, // Stating the element for Errors
       },
       { path: "/cart", element: <Cart /> },
-      { path: "/order/new", element: <CreateOrder /> },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+        action: createOrderAction,
+      },
       { path: "/order/:orderId", element: <Order />, loader: orderLoader },
     ],
   },
--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { Form, redirect } from "react-router-dom";
+import { createOrder } from "../../services/apiRestaurant";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -38,7 +40,7 @@ function CreateOrder() {
     <div>
       <h2>Ready to order? Let's go!</h2>
 
-      <form>
+      <Form method="POST">
         <div>
           <label>First Name</label>
           <input type="text" name="customer" required />
@@ -66,15 +68,37 @@ function CreateOrder() {
             // value={withPriority}
             // onChange={(e) => setWithPriority(e.target.checked)}
           />
-          <label htmlFor="priority">Want to yo give your order priority?</label>
+          <label htmlFor="priority">Want to give your order priority?</label>
         </div>
 
         <div>
+          <input type="hidden" name="cart" value={JSON.stringify(cart)} />
           <button>Order now</button>
         </div>
-      </form>
+      </Form>
     </div>
   );
+}
+
+// Function that will intercept the form's request (needs to be connected to the route)
+export async function action({ request }) {
+  const formData = await request.formData(); // Getting the formData from the request that contains all the form fields
+  const data = Object.fromEntries(formData); // Converting the object to the simplier object
+
+  // Getting the new object with some overwrites
+  const order = {
+    ...data,
+    cart: JSON.parse(data.cart), // Converting the cart back to be an object
+    priority: data.priority === "on", // Priority, should be boolean
+  };
+
+  // Sending the order with all the details to the API
+  const newOrder = await createOrder(order);
+
+  // Redirecting user to the order tracking page
+  // *If we return the new response from the action the router will go to the URL that is contained in the response
+  // *redirect function does returns new response
+  return redirect(`/order/${newOrder.id}`);
 }
 
 export default CreateOrder;
