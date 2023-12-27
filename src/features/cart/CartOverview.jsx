0d79179dbfc3e26881d0d142876ab6b088f3451c
vsbron <vsbron@gmail.com>
2023-12-27 15:17:08 +0300

    Added some basic borders and display properties

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -2,9 +2,10 @@ import { Link } from "react-router-dom";
 
 function CartOverview() {
   return (
-    <div className="bg-stone-800 uppercase text-stone-200">
-      <p className="font-semibold text-stone-300">
+    <div className="bg-stone-800 p-4 uppercase text-stone-200">
+      <p className="space-x-4 font-semibold text-stone-300">
         <span>23 pizzas</span>
+        <span>$23.45</span>
         <span>$23.45</span>
       </p>
       <Link to="/cart">Open cart &rarr;</Link>
--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="text-sm font-semibold">BroN</div>;
+  return <div className="hidden text-sm font-semibold">BroN</div>;
 }
 
 export default Username;
