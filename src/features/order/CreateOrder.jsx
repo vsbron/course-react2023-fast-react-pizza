11dae6f80182cc277bcd4ed439b7f377834fbde6
vsbron <vsbron@gmail.com>
2023-12-28 15:41:00 +0300

    Added basic buttons styling

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -82,7 +82,10 @@ function CreateOrder() {
         <div>
           {/* Converting the cart to the string */}
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button disabled={isSubmitting}>
+          <button
+            disabled={isSubmitting}
+            className="inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-stone-300"
+          >
             {isSubmitting ? "Placing order..." : "Order now"}
           </button>
         </div>
