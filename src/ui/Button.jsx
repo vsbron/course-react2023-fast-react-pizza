7e4bc3bb04c7c5eadce320cd7cefa0142b9bc514
vsbron <vsbron@gmail.com>
2024-01-03 17:08:26 +0300

    Styled cart page + Added 1 style type to the Buton component

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -8,6 +8,8 @@ function Button({ children, disabled, to, type }) {
 
   const styles = {
     primary: base + " px-4 py-3 md:px-6 md:py-4",
+    secondary:
+      "inline-block rounded-full border-2 border-stone-300 font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 focus:text-stone-800 focus:bg-stone-300 disabled:cursor-not-allowed disabled:bg-stone-300 px-4 py-2.5 md:px-6 md:py-3.5",
     small: base + " px-4 py-2 sm:px-5 sm:py-2.5 text-xs",
   };
 
