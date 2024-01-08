8732e94122d5ed24531c2401978c08f911c9e09c
vsbron <vsbron@gmail.com>
2024-01-08 14:38:11 +0300

    Completed

--- src/ui/Footer.jsx
+++ src/ui/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer className="border-t border-stone-200 bg-yellow-400 px-4 py-2 text-right text-xs">
       Built by VSBroN as a part of the React online learning course | ©2024 All
-      rights reserved.{" "}
+      rights reserved
     </footer>
   );
 }
